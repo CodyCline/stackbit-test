3a7adfd3f5cc0a8dce376f20446b85e52d5e3c99
CodyCline <20631166+CodyCline@users.noreply.github.com>
2024-06-20 14:27:22 -0700

    remove contentful

--- stackbit.config.js
+++ stackbit.config.js
@@ -8,20 +8,6 @@ export default {
 	ssgName: 'next',
 	nodeVersion: '18',
   buildCommand: 'npm run  build',
-  contentSources: [
-    new ContentfulContentSource({
-      spaceId: process.env.CONTENTFUL_SPACE_ID_01,
-      environment: process.env.CONTENTFUL_ENVIRONMENT_01,
-      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN_01,
-      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN_01
-    }),
-    new ContentfulContentSource({
-      spaceId: process.env.CONTENTFUL_SPACE_ID_02,
-      environment: process.env.CONTENTFUL_ENVIRONMENT_02,
-      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN_02,
-      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN_02
-    })
-  ]
   // ...
 };
 
